e52efa7458322c42881432d29cc8f06f9d477b91
Reinaldo Neto <reinaldonetof@hotmail.com>
2025-09-03 19:04:21 -0300

    feat: implement navigation with Home and Detail pages using React Navigation

---
+++ src/screens/DetailPage/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+// import { Container } from './styles';
+
+const DetailPage: React.FC = () => {
+  return <View />;
+};
+
+export default DetailPage;
